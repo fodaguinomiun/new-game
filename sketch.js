9d134db19183e628ff3f04f62b7a7481a84c4ede
guinomiun <120798892+fodaguinomiun@users.noreply.github.com>
2022-12-22 14:11:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var players, block, ball
+var Play
+var gameState
+var lebron
+var curry
+var player
+var irving
+var durant
+var hortencia
+var lessie
+
+
+
+
+
+function draw(){
+    background(0)
+    if(gameState===0){
+    Play()
+ //machine()
+ for(var i; i < player.length;i++){
+    if(player.onPress("up")){
+    player
+    }
+ }
+    }
+}
